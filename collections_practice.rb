4666621972ce0030c04ff05c0a2882cef57c9c43
Meg Galvez <meg@oakmaine.com>
2019-04-18 15:43:03 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -41,6 +41,6 @@ def sum_array(array)
 end 
 
 def add_s(array)
-  array.each_with_index.collect {|element, index| 
-
+  array.each_with_index.collect do |element, index| 
+    if element == 1 
 end 
