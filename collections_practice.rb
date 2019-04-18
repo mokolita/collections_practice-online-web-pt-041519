6bb6c1cd1d05400db7b028176f8d1a3a95f84086
Meg Galvez <meg@oakmaine.com>
2019-04-18 14:19:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,19 @@
+def sort_array_asc(int)
+  int.sort 
+end 
 
+def sort_array_desc(int)
+  int.sort {|a, b| b <=> a }
+
+end 
+
+def sort_array_char_count(int)
+  int.sort {|string_1, string_2| string_1.length <=> string_2.length }
+end 
+
+def swap_elements(element)
+  element.sort do |i_1, i_2|
+    element[1] <=> element[2]
+  end 
+
+end 
